ef392ead629d8db61cf6f5baa0fe4590a4ac752e
e <kangwei.chen@tcs.com>
2017-06-09 17:44:40 +0800

    星级评价提交
    星级评价组件修改

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,31 +1,40 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-stars',
   templateUrl: './stars.component.html',
   styleUrls: ['./stars.component.css']
 })
-export class StarsComponent implements OnInit {
+export class StarsComponent implements OnInit, OnChanges {
 
   @Input()
   rating: number = 0;
 
   stars: boolean[];
 
+  @Input()
+  readyOnly: boolean = true;
+
+  @Output()
+  ratingChange: EventEmitter<number> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
     this.stars = [];
     for (let i = 1; i <= 5; i++ ) { // Math.round(this.rating)
       this.stars.push(i > this.rating);
     }
+
   }
 
   clickStar(index: number) {
-    this.stars = [];
-    this.rating = index + 1;
-    for (let i = 1; i <= 5; i++ ) { // Math.round(this.rating)
-      this.stars.push(i > this.rating);
+    if (!this.readyOnly) {
+      this.rating = index + 1;
+      this.ratingChange.emit(this.rating);
     }
   }
 
