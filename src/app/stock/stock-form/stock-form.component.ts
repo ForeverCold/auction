23c922dee6a6afbe7864df5d914472501ad8f36b
e <kangwei.chen@tcs.com>
2017-06-12 17:59:55 +0800

    表单提交
    表单功能优化

--- src/app/stock/stock-form/stock-form.component.ts
+++ src/app/stock/stock-form/stock-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Stock, StockService} from '../stock.service';
 import {ActivatedRoute, Router} from '@angular/router';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-stock-form',
@@ -9,7 +10,11 @@ import {ActivatedRoute, Router} from '@angular/router';
 })
 export class StockFormComponent implements OnInit {
 
+  formModel: FormGroup;
+
   stock: Stock;
+
+  categories = ['IT', '互联网', '金融'];
 
   constructor(private routeInfo: ActivatedRoute, private stockservice: StockService,
     private router: Router
@@ -18,6 +23,18 @@ export class StockFormComponent implements OnInit {
   ngOnInit() {
     const stockId = this.routeInfo.snapshot.params['id'];
     this.stock = this.stockservice.getstock(stockId);
+
+    const fb = new FormBuilder();
+    this.formModel = fb.group({
+      name: [this.stock.name, [Validators.required, Validators.minLength(3)]],
+      price: [this.stock.price, [Validators.required]],
+      desc: [this.stock.desc],
+      categories: fb.array([
+        new FormControl(),
+        new FormControl(),
+        new FormControl()
+      ])
+    });
   }
 
   cancel()　{
@@ -25,7 +42,7 @@ export class StockFormComponent implements OnInit {
   }
 
   save() {
-    console.log(this.stock.rating);
+    console.log(this.formModel.value);
     this.router.navigateByUrl('/stock');
   }
 
